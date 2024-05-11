6b1ad385646f7341181ca038aa1a0df29c413237
Alexandr Yaskevich <yask.alexandr@gmail.com>
2024-05-11 16:49:32 +0300

    objectProxy second work disrtuctur

--- src/js/funcdiscr.js
+++ src/js/funcdiscr.js
@@ -2,24 +2,23 @@ import { character } from "../character";
 
 export default function Distr (character) {
     const arrayAtack = [];
-    const objec = {};
-    for (const atack in character.special) {
-        if(atack == "id") {
-            objec["id"] = character.special[atack]
-        }
-        if(atack == "name") {
-            objec["name"] = character.special[atack];  
-        }
-        if(atack == "description") {
-            objec["description"] = character.special[atack];  
-        }
-        if(!character.special.hasOwnProperty('description')) {
-            objec["description"] = 'Описание недоступно';
-        }
-        if(atack == "icon") {
-            objec["icon"] = character.special[atack];
-        }
 
-        return arrayAtack.push(objec);
+    for (const atack in character.special) {
+       const {
+        id,
+        name,
+        description,
+        icon
+       } = atack;
+       arrayAtack.push({        
+        id,
+        name,
+        description: description !== undefined ? description : 'Описание недоступно',
+        icon
+    })
+
+        return arrayAtack;
     }
 }
+
+
